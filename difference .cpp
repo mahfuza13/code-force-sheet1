bf2b521e8853866701bc8ef672bd65b3a2cbb5ba
Mahfuza Akther <mahfuzaakther2004@gmail.com>
2025-09-15 13:40:18 +0600

    add my code force sheet1

---
+++ difference .cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std ;
+int main ()
+{
+    long long A , C ;
+    int B , D ;
+    cin >> A >> B >> C >> D ;
+    long long x = (A*B) - (C*D) ;
+    cout << "Difference = " << x << endl ;
+    return 0 ;
+
+}
---
+++ digits sumation.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std ;
+int main()
+{
+    long long m,n ;
+    cin >> m >> n ;
+    long long dsum = (m%10) + (n%10) ;
+    cout << dsum << endl ;
+    return 0;
+
+}
