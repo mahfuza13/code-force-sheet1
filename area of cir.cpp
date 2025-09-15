5e08c0dde54fbebe0ab03605c141a33dfbd24a01
Mahfuza Akther <mahfuzaakther2004@gmail.com>
2025-09-15 14:03:16 +0600

    add my code force sheet1

---
+++ area of cir.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std ;
+int main ()
+{
+    double R;
+    const double pie = 3.141592653 ;
+    cin >> R ;
+    double area = pie * R * R ;
+    cout << fixed << setprecision (9) ;
+    cout << area << endl ;
+    return 0 ;
+
+}
