ddaab7f779cc34012112c5b037caa82f961d9910
Mahfuza Akther <mahfuzaakther2004@gmail.com>
2025-09-18 01:25:35 +0600

    add my code force sheet1

---
+++ Age in Days.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std ;
+int main ()
+{
+    long long n ;
+    cin >> n ;
+    cout << (n/365) << " years" << endl ;
+    n = n%365 ;
+    cout << (n/30) << " months" << endl ;
+    n = n%30 ;
+    cout << n << " days" << endl ;
+
+    return 0 ;
+
+}
