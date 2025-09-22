f236197bfd9f4928888dbfe6d2175078a55c2245
Mahfuza Akther <mahfuzaakther2004@gmail.com>
2025-09-22 12:06:01 +0600

    add my code force sheet 1

---
+++ Two Interval.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std ;
+int main ()
+{
+    int l1 , r1 , l2 , r2 ;
+    cin >> l1 >> r1 >> l2 >> r2 ;
+    int l = max(l1,l2) ;
+    int r = min(r1,r2) ;
+    if (max(l1,l2)<=min(r1,r2))
+    {
+        cout << l << " " << r << endl ;
+    }
+    else {
+        cout << "-1" << endl ;
+    }
+    return 0 ;
+}
